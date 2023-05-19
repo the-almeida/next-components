c3775e3b26cf3bb3b86c29c9efe5dc597183c170
Gustavo Almeida <gspalmeida@icloud.com>
2023-05-19 09:18:15 -0300

    feat: Basic Form + Custom Inputs with validation

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,91 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+'use client'
+
+import Input from '@/components/Form/Input'
+import {v4 as uuidV4} from 'uuid'
+import { useState } from 'react'
+
+import styles from './home.module.css'
+
+interface FormValues {
+  fullName: string;
+  email: string;
+  phoneNumber: string;
+  companyName: string;
+}
 
 export default function Home() {
+  const [formValues, setFormValues] = useState<FormValues>({
+    fullName: "",
+    email: "",
+    phoneNumber: "",
+    companyName: "",
+  })
+
+  const inputs = [
+    {
+      key: uuidV4().toString(),
+      name: "fullName",
+      type: "text",
+      placeholder: "Ex: Gustavo Almeida",
+      label: "Nome",
+      errorMessage: "O nome deve ter entre 3-16 caracteres e não deve incluir caracteres especiais.",
+      pattern: "^[A-Za-z ]{3,16}$",
+      required: true
+    },
+    {
+      key: uuidV4().toString(),
+      name: "email",
+      type: "email",
+      placeholder: "Ex: nome.sobrenome@thealmeida.online",
+      label: "Email",
+      errorMessage: "Deve ser um email válido.",
+      required: true
+    },
+    {
+      key: uuidV4().toString(),
+      name: "phoneNumber",
+      type: "text",
+      placeholder: "Ex: (44) 91234-5678",
+      label: "Telefone/Whatsapp",
+      errorMessage: "O telefone deve ter entre 10-21 caracteres. Lembre-se de adicionar o DDD, ex: 44 9 9977-6655",
+      pattern: "^[0-9\-\+() ]{10,25}$",
+      required: true
+    },
+    {
+      key: uuidV4().toString(),
+      name: "companyName",
+      type: "text",
+      placeholder: "Ex: GalileoSoft",
+      label: "Nome da Empresa",
+      errorMessage: "O nome da empresa deve ter entre 3-16 caracteres."
+    },
+  ]
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) =>{
+    e.preventDefault()
+  }
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormValues({...formValues, [e.target.name]: e.target.value})
+  }
+  
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
+    <main className={styles.app}>
+      <h1>Simple form using custom Inputs with validation and mask:</h1>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        {
+          inputs.map(({key, ...defaultInputProps}) => (
+            <Input 
+              key={key}
+              id={key}
+              {...defaultInputProps}
+              value={formValues[defaultInputProps.name as keyof FormValues]}
+              onChange={onChange}
             />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          ))
+        }
+        <button type="submit">Enviar</button>
+      </form>
     </main>
   )
 }
---
+++ components/Form/Input.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+
+import styles from './input.module.css';
+
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+    label: string,
+    errorMessage: string,
+}
+
+
+const Input = (props: InputProps) => {
+    const [focused, setFocused] = useState(false)
+    const { label, onChange, errorMessage, id, ...inputProps } = props
+
+    const handleBlur = () => {
+        setFocused(true)
+    }
+    
+    return (
+        <div className={styles.inputContainer}>
+            <label htmlFor={id} >{label}</label>
+            <input 
+                id={id}
+                onChange={onChange}
+                onBlur={handleBlur}
+                x-focused={focused.toString()}
+                {...inputProps}
+            />
+            <span>{errorMessage}</span>
+        </div>
+    )
+}
+
+export default Input
